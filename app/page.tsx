fd042cdc8aee93af2a66de1019bf46e771dee7dd
kaffucino0-wq <kaffu.cino0@gmail.com>
2026-04-03 10:55:08 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "black",
+      color: "white"
+    }}>
+      <h1>Kenn Portfolio 🚀</h1>
+    </main>
+  );
+}
